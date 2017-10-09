69d68f1808c212ae727315504c75e58a4b7db78b
Fabio Baltieri <fabio.baltieri@gmail.com>
2017-10-09 20:07:32 +0100

    fw: add basic button handling

--- firmware-remote/main.c
+++ firmware-remote/main.c
@@ -29,7 +29,9 @@ static void reset_cpu(void)
 
 struct nrf_frame frm;
 static uint8_t seq = 0;
-static uint8_t flags = 0x00;
+static uint8_t flags;
+static uint8_t tx_on;
+static volatile uint8_t retransmit;
 
 static void send_frame(void) {
 	struct nrf_bike_lights *nbl = &frm.msg.bike_lights;
@@ -52,6 +54,64 @@ ISR(PCINT0_vect)
 ISR(PCINT1_vect)
 {
 	/* Switches, just to wake up the main loop. */
+	retransmit = 1;
+}
+
+ISR(WDT_vect)
+{
+	/* Wake up the main loop. */
+	retransmit = 1;
+}
+
+static void toggle_on_off(void)
+{
+	tx_on ^= 1;
+
+	if (tx_on) {
+		/* WDT as periodic interrupt */
+		WDTCSR |= (1 << WDE) | (1 << WDCE);
+		WDTCSR = (0 << WDCE) | (1 << WDIF) | (1 << WDIE) |
+			(1 << WDP2) | (1 << WDP1) | (0 << WDP0);
+	} else {
+		wdt_disable();
+	}
+}
+
+static void update_leds(void)
+{
+
+	if (flags & NRF_BL_REAR_BLINKING)
+		led3_on();
+	else
+		led3_off();
+
+	if (flags & NRF_BL_FRONT_BEAM)
+		led4_on();
+	else
+		led4_off();
+
+	if (flags & NRF_BL_REAR_BEAM)
+		led1_on();
+	else
+		led1_off();
+}
+
+static void blink(void)
+{
+	led2_on();
+	_delay_ms(33);
+	led2_off();
+}
+
+#define DEBOUNCE_DELAY_MS 50
+static void wait_release(void)
+{
+	uint8_t c;
+	for (c = DEBOUNCE_DELAY_MS; c; c--) {
+		if (sw1_read() || sw2_read() || sw3_read())
+			c = DEBOUNCE_DELAY_MS;
+		_delay_ms(1);
+	}
 }
 
 static void hello(void)
@@ -93,22 +153,32 @@ int __attribute__((noreturn)) main(void)
 
 	sei();
 	for (;;) {
+		_delay_ms(2);
+
 		if (sw1_read())
-			led2_on();
-		else
-			led2_off();
-		if (sw2_read())
-			led3_on();
-		else
-			led3_off();
+			toggle_on_off();
+
+		if (sw2_read()) {
+			flags ^= NRF_BL_REAR_BLINKING;
+			flags ^= NRF_BL_FRONT_STEADY;
+		}
+
 		if (sw3_read())
-			led4_on();
-		else
-			led4_off();
+			flags ^= NRF_BL_FRONT_BEAM;
+
 		if (sw4_read())
-			led1_on();
+			flags |= NRF_BL_REAR_BEAM;
 		else
-			led1_off();
+			flags &= ~NRF_BL_REAR_BEAM;
+
+		if (retransmit) {
+			update_leds();
+			blink();
+			send_frame();
+			retransmit = 0;
+		}
+
+		wait_release();
 
 		set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 		sleep_mode();
